e2cc57fc6eebc32d2696f052277e22c13af05ebd
VIP-HACKER <griss_hai2005@hotmail.com>
2023-03-10 13:53:30 +0100

    Added Server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+/*Configuration*/
