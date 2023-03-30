ab357cc9f078ef16ede139a70339ba98d0892517
VIP-HACKER <griss_hai2005@hotmail.com>
2023-03-30 13:31:32 +0200

    Eroors
    First show app Dashboard

--- Dashboard/client/src/index.js
+++ Dashboard/client/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { configureStore } from '@reduxjs/toolkit';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
 import globalReducer from "state";
 import { Provider } from "react-redux";
 
@@ -12,7 +12,7 @@ const store = configureStore({
   },
 })
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
