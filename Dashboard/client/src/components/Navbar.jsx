74ba45a1371653316ad75672f557eef44cd90a9f
VIP-HACKER <griss_hai2005@hotmail.com>
2023-03-31 04:33:23 +0200

    Left Side(search...)
    59:11 Time

--- Dashboard/client/src/components/Navbar.jsx
+++ Dashboard/client/src/components/Navbar.jsx
@@ -1,34 +1,50 @@
-import React, { useState } from 'react';
-import { LightModeOutlined, DarkModeOutlined, Menu as MenuIcon, Search, SettingsOutlined, ArrowDropDownOutlined } from '@mui/icons-material';
-import FlexBetween from 'components/FlexBetween';
-import { useDispatch } from 'react-redux';
-import { setMode } from 'state';
+import React, { useState } from "react";
+import { LightModeOutlined, DarkModeOutlined, Menu as MenuIcon, Search, SettingsOutlined, ArrowDropDownOutlined, } from "@mui/icons-material";
+import FlexBetween from "components/FlexBetween";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
 import profileImage from "assets/profile.jpeg";
-import { AppBar, IconButton, Toolbar, useTheme } from '@mui/material';
+import { AppBar, Button, Box, Typography, IconButton, InputBase, Toolbar, Menu, MenuItem, useTheme, } from "@mui/material";
 
-const Navbar = () => {
+
+const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
     const dispatch = useDispatch();
     const theme = useTheme();
-
-    return <AppBar
-        sx={{
-            position: "static",
-            background: "none",
-            boxShadow: "none",
-        }}
-    >
-        <Toolbar sx={{ justifyContent: "space-between" }}>
-        {/**LEFT SIDE */}
-<FlexBetween>
- 
-
-    
-</FlexBetween>
+    return (
+        <AppBar
+            sx={{
+                position: "static",
+                background: "none",
+                boxShadow: "none",
+            }}
+        >
+            <Toolbar sx={{ justifyContent: "space-between" }}>
 
 
 
-        </Toolbar>
-    </AppBar>;
 
+
+
+                {/**LEFT SIDE */}
+
+                <FlexBetween>
+                    <IconButton onClick={() => console.log('open/close sideBar')}>
+                        <MenuIcon />
+                    </IconButton>
+                    <FlexBetween
+                        backgroundColor={theme.palette.background.alt}
+                        borderRadius="9px"
+                        gap="3rem"
+                        p="0.1rem 1.5rem"
+                    >
+                        <InputBase placeholder="Search..." />
+                        <IconButton>
+                            <Search />
+                        </IconButton>
+                    </FlexBetween>
+                </FlexBetween>
+            </Toolbar>
+        </AppBar>
+    );
 };
 export default Navbar;
