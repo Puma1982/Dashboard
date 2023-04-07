f0104fb3c93d33bfba302efa4af31cb86ccc0218
VIP-HACKER <griss_hai2005@hotmail.com>
2023-04-07 13:52:41 +0200

    Sidebar+burger
    1:29:17

--- Dashboard/client/src/components/Sidebar.jsx
+++ Dashboard/client/src/components/Sidebar.jsx
@@ -33,6 +33,65 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import FlexBetween from './FlexBetween';
 import profileImage from 'assets/profile.jpeg';
 
+const navItems = [
+  {
+    text: 'Grissander-Dashboard',
+    icon: <HomeOutlined />,
+  },
+  {
+    text: 'Client Part',
+    icon: null,
+  },
+  {
+    text: 'Products',
+    icon: <ShoppingCartOutlined />,
+  },
+  {
+    text: 'Customers',
+    icon: <Groups2Outlined />,
+  },
+  {
+    text: 'Transactions',
+    icon: <ReceiptLongOutlined />,
+  },
+  {
+    text: 'Geography',
+    icon: <PublicOutlined />,
+  },
+  {
+    text: 'Sales',
+    icon: null,
+  },
+  {
+    text: 'Overview',
+    icon: <PointOfSaleOutlined />,
+  },
+  {
+    text: 'Daily',
+    icon: <TodayOutlined />,
+  },
+  {
+    text: 'Monthly',
+    icon: <CalendarMonthOutlined />,
+  },
+  {
+    text: 'Breakdown',
+    icon: <PieChartOutlined />,
+  },
+  {
+    text: 'Managment',
+    icon: null,
+  },
+  {
+    text: 'Admin',
+    icon: <AdminPanelSettingsOutlined />,
+  },
+  {
+    text: 'Performance',
+    icon: <TrendingUpOutlined />,
+  },
+];
+
 const Sidebar = ({
   user,
   drawerWidth,
@@ -69,25 +128,73 @@ const Sidebar = ({
           }}
         >
           <Box width="100%">
-            <Box m="1.5rem 2rem 3rem">
+            <Box m="1.5rem 2rem 2rem 3rem">
               <FlexBetween color={theme.palette.secondary.main}>
-                <Box display="flex" alignItems="center" gap="0.5rem ">
-                    <Typography  variant='h4' fontWeight= "bold">
-                        I-C-U
-                    </Typography>
+                <Box display="flex" alignItems="center" gap="0.5rem">
+                  <Typography variant="h4" fontWeight="bold">
+                    I-C-U
+                  </Typography>
                 </Box>
                 {!isNonMobile && (
-                    <IconButton onClick={() => setIsSidebarOpen(! isSidebarOpen)}>
-                        <ChevronLeft />
-                    </IconButton>
+                  <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                    <ChevronLeft />
+                  </IconButton>
                 )}
               </FlexBetween>
             </Box>
+            <List>
+              {navItems.map(({ text, icon }) => {
+                if (!icon) {
+                  return (
+                    <Typography key={text} sx={{ m: '2.25rem 0 1rem 3rem' }}>
+                      {text}
+                    </Typography>
+                  );
+                }
+                const lcText = text.toLowerCase();
+
+                return (
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton
+                      onClick={() => {
+                        navigate(`/${lcText}`);
+                        setActive(lcText);
+                      }}
+                      sx={{
+                        backgroundColor:
+                          active === lcText
+                            ? theme.palette.secondary[300]
+                            : 'transparent',
+                        color:
+                          active === lcText
+                            ? theme.palette.primary[600]
+                            : theme.palette.secondary[100],
+                      }}
+                    >
+                      <ListItemIcon
+                        sx={{
+                          ml: '2rem',
+                          color:
+                            active === lcText
+                              ? theme.palette.primary[600]
+                              : theme.palette.secondary[200],
+                        }}
+                      >
+                        {icon}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                      {active === lcText && (
+                        <ChevronRightOutlined sx={{ ml: 'auto' }} />
+                      )}
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+            </List>
           </Box>
         </Drawer>
       )}
     </Box>
   );
 };
-
 export default Sidebar;
